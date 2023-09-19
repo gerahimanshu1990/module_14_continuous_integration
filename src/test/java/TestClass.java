eca188e114d5c88f7c8de0b8d059927671cec5bb
Himanshu Gera <gera.himanshu1990@gmail.com>
2023-09-19 14:45:03 +0530

    Update TestClass.java

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,3 +1,4 @@
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -11,6 +12,11 @@ public class TestClass {
     @Test
     public void test() {
         System.out.println("In Test Method :: ");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("In After Method :: ");
     }
 
 }
